4a6431811bf2cceac4a57c0558e3f315ceddace8
miau12333 <juta2303@gmail.com>
2022-09-18 18:15:04 -0500

    ya

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,32 @@
-import { useState } from 'react';
-import UsersForm from './components/UsersForm';
-import UsersList from './components/UsersList';
-import './App.css';
-import { useEffect } from 'react';
-import axios from 'axios';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
 function App() {
-  
-  const[dataUsers, setDataUsers] = useState([]);
-  const[userSelected, setUserSelect] = useState(null); 
-  useEffect(() => {
-    axios.get('https://users-crud1.herokuapp.com/users/')
-    .then(res => setDataUsers(res.data))
-  }, []);
-
-  console.log(dataUsers);
-
-  const getUsers = () => {
-    axios.get('https://users-crud1.herokuapp.com/users/')
-    .then(res => setDataUsers(res.data))
-  }
-
-  const addUsers = (newUser) => { 
-    axios.post('https://users-crud1.herokuapp.com/users/', newUser)
-    .then(() => getUsers()); 
-  }
-
-  const editUsers = (datauser) => {
-    setUserSelect(datauser); 
-  };
-
-  const updateUsers = (newUser) => {
-    axios
-      .put(`https://users-crud1.herokuapp.com/users/${newUser.id}/`, newUser)
-      .then(() => getUsers())
-      .catch((error) => console.log(error.response));
-  };
-
-  const deleteUsers= (idDelete) => {
-    axios
-      .delete(`https://users-crud1.herokuapp.com/users/${idDelete}/`, idDelete)
-      .then(() => getUsers());
-  };
+  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <div>
-        <div className='modal'>
-          <div className='container'>
-          <UsersForm 
-            addUser={addUsers}
-            userSelected={userSelected}
-            updateUser={updateUsers}
-          />
-          </div>
-        </div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <UsersList 
-        dataUsers={dataUsers} 
-        editUser={editUsers}
-        deleteUser={deleteUsers}
-      />
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
   )
 }
